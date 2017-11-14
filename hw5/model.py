b53eede4131270ecb3af8c9c4e99fe59ec93c227
GaryQian <gary@qian.ninja>
2017-11-13 22:52:02 -0500

    Decoder start

--- hw5/model.py
+++ hw5/model.py
@@ -29,10 +29,46 @@ class Encoder(nn.Module):
     self.lstm.bias_hh_l0_reverse.data = self.initParams["encoder.rnn.bias_hh_l0_reverse"]
     
 
-  def forward(self, input, do_dropout=False):
+  def forward(self, input, hidden=None):
     embedded = self.embedding(input).view(1, 1, -1)
     output = embedded
-    output, hidden = self.lstm(output, None)
-    return output
+    output, hidden = self.lstm(output, hidden)
+    return output, hidden
+
+
+class Decoder(nn.Module):
+  def __init__(self):
+    super(Decoder, self).__init__()
     
-m = NMT()
+    self.initParams = torch.load(open("model.param", "rb"))
+    
+    self.embedding = torch.nn.Embedding(23262, 300)
+    self.embedding.weight.data = self.initParams["decoder.embeddings.emb_luts.0.weight"]
+    
+    self.lstm = torch.nn.LSTM(input_size=4096, hidden_size=1024, num_layers=1)
+    
+    self.lstm.weight_ih_l0.data = self.initParams["decoder.rnn.layers.0.weight_ih"]
+    self.lstm.weight_hh_l0.data = self.initParams["decoder.rnn.layers.0.weight_hh"]
+    self.lstm.bias_ih_l0.data = self.initParams["decoder.rnn.layers.0.bias_ih"]
+    self.lstm.bias_hh_l0.data = self.initParams["decoder.rnn.layers.0.bias_hh"]
+    
+    self.attin = torch.nn.Linear(1024, 1024)
+    self.attin.weight.data = self.initParams["decoder.attn.linear_in.weight"]
+    
+    self.softmax = nn.LogSoftmax()
+    
+    self.attout = torch.nn.Linear(1024, 2048)
+    self.attout.weight.data = self.initParams["decoder.attn.linear_out.weight"]
+    
+
+  def forward(self, input, hidden=None):
+    embedded = self.embedding(input).view(1, 1, -1)
+    output = embedded
+    output, hidden = self.lstm(output, hidden)
+    output = self.softmax(self.attin(output))
+    output = self.attout(output)
+    return output, hidden
+    
+
+m = Encoder()
+m = Decoder()
