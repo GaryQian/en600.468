d48ac5581984a961a1579d051d5af139234af83e
GaryQian <gary@qian.ninja>
2017-11-17 15:17:50 -0500

    Fix memory leak in encoder

--- hw5/modelgary2.py
+++ hw5/modelgary2.py
@@ -14,51 +14,16 @@ class Encoder(nn.Module):
     self.embedding = torch.nn.Embedding(vocab_size, 300)
 
     self.lstm = torch.nn.LSTM(input_size=300, hidden_size=512, num_layers=1, bidirectional=True)
-    
-    self.hidden = None
-    
+
 
   def forward(self, input):
     embedded = self.embedding(input)
     output = embedded
-    output, self.hidden = self.lstm(output, self.hidden)
-    return output, self.hidden
-
-    
-class Attn(nn.Module):
-  def __init__(self, method, hidden_size):
-      super(Attn, self).__init__()
-      
-      self.method = method
-      self.hidden_size = hidden_size
-      
-      self.softmax = nn.LogSoftmax()
-      
-      self.attn = nn.Linear(self.hidden_size, hidden_size)
-
-  def forward(self, hidden, encoder_outputs):
-    max_len = encoder_outputs.size(0)
-    this_batch_size = encoder_outputs.size(1)
-
-    # Create variable to store attention energies
-    attn_energies = Variable(torch.zeros(this_batch_size, max_len)) # B x S
-
-    # For each batch of encoder outputs
-    for b in range(this_batch_size):
-      # Calculate energy for each encoder output
-      for i in range(max_len):
-        attn_energies[b, i] = self.score(hidden[:, b], encoder_outputs[i, b].unsqueeze(0))
-
-    # Normalize energies to weights in range 0 to 1, resize to 1 x B x S
-    return self.softmax(attn_energies).unsqueeze(1)
-  
-    def score(self, h_s, h_t):
-        h_t = self.attin(h_t)
-        return h_t.unsqueeze(1).bmm(h_s.unsqueeze(1).transpose(1,2))
+    output, hidden = self.lstm(output, None)
+    return output, hidden
 
 class Decoder(nn.Module):
   def _init_state(self, encoder_hidden):
-    """ Initialize the encoder hidden state. """
     if encoder_hidden is None:
       return None
     if isinstance(encoder_hidden, tuple):
@@ -68,9 +33,6 @@ class Decoder(nn.Module):
     return encoder_hidden
     
   def _cat_directions(self, h):
-    """ If the encoder is bidirectional, do the following transformation.
-        (#directions * #layers, #batch, hidden_size) -> (#layers, #batch, #directions * hidden_size)
-    """
     h = torch.cat([h[0:h.size(0):2], h[1:h.size(0):2]], 2)
     return h
 
@@ -84,11 +46,7 @@ class Decoder(nn.Module):
     
     self.embedding = torch.nn.Embedding(trg_vocab_size, 300)
     
-    
-    self.attn = Attn('general', self.hidden_size)
-    
     self.attin = torch.nn.Linear(self.hidden_size, self.hidden_size)
-    self.softmax = nn.LogSoftmax()
     
     self.attout = torch.nn.Linear(2048, self.hidden_size)
     
@@ -97,10 +55,6 @@ class Decoder(nn.Module):
     
     #23262
     self.gen = torch.nn.Linear(self.hidden_size,trg_vocab_size)
-    self.hidden = None#Parameter(torch.randn((48, 1024)))
-    self.prev = Variable(torch.randn((48, self.hidden_size)))
-    
-    self.concat = nn.Linear(self.hidden_size * 2, self.hidden_size)
     
 
   def forward(self, targ, encoder_out, encoder_hidden):
@@ -132,17 +86,7 @@ class Decoder(nn.Module):
     #print output
     predicted_softmax = F.log_softmax(self.gen(output.view(-1, self.hidden_size))).view(batch_size, self.output_size, -1)
     return predicted_softmax#, hidden, attn
-    
 
-    #self.prev, _= self.lstm(output, None)#Variable(torch.zeros((2, 48, 1024,))))
-    #generated = self.gen(self.prev)
-    #self.prev = self.prev[-1]
-    #return generated
-
-  #def score(self, h_s, h_t):
-  #  h_t = self.attin(h_t)
-  #  res= h_t.unsqueeze(1).bmm(h_s.unsqueeze(1).transpose(1,2))
-  #  return res
     
 class NMT(nn.Module):
   def __init__(self, src_vocab_size, trg_vocab_size):
